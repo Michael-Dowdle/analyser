2794e61d967096767dc54858b9b4d2c34918eac5
Simba-Cream <michaeldowdle@msn.com>
2018-02-26 20:24:29 +0000

    implement public, private and protected handling of class atributes.

--- analyser/Statistic.py
+++ analyser/Statistic.py
@@ -3,10 +3,18 @@ import collections
 import re
 
 
-class StatisticBase(ABC):
+class _StatisticBase(ABC):
     def __init__(self):
-        self.result = ""
+        self._result = ""
         super().__init__()
+
+    @property
+    def result(self):
+        return self._result
+
+    @result.setter
+    def result(self, value):
+        self._result = value
 
     @abstractmethod
     def process_line(self, line):
@@ -17,59 +25,59 @@ class StatisticBase(ABC):
         pass
 
     def get_result(self):
-        return self.result
+        return self._result
 
 
-class WordCountStatistic(StatisticBase):
+class WordCountStatistic(_StatisticBase):
     def __init__(self):
-        self.wordCount = 0
+        self.__wordCount = 0
         super().__init__()
 
     def process_line(self, line):
-        self.wordCount += len(line.split())
+        self.__wordCount += len(line.split())
 
     def calculate(self):
-        self.result = self.wordCount
+        self.result = self.__wordCount
 
 
-class LineCountStatistic(StatisticBase):
+class LineCountStatistic(_StatisticBase):
     def __init__(self):
-        self.lineCount = 0
+        self.__lineCount = 0
         super().__init__()
 
     def process_line(self, *_):
-        self.lineCount += 1
+        self.__lineCount += 1
 
     def calculate(self):
-        self.result = self.lineCount
+        self.result = self.__lineCount
 
 
-class AvgLettersPerWordStatistic(StatisticBase):
+class AvgLettersPerWordStatistic(_StatisticBase):
     def __init__(self):
-        self.wordCount = 0
-        self.totalWordLengths = 0
+        self.__wordCount = 0
+        self.__totalWordLengths = 0
         super().__init__()
 
     def process_line(self, line):
         for word in line.split():
-            self.wordCount += 1
-            self.totalWordLengths += len(word)
+            self.__wordCount += 1
+            self.__totalWordLengths += len(word)
 
     def calculate(self):
-        self.result = round(self.totalWordLengths / self.wordCount, 1)
+        self.result = round(self.__totalWordLengths / self.__wordCount, 1)
 
 
-class MostCommonLetterStatistic(StatisticBase):
+class MostCommonLetterStatistic(_StatisticBase):
     def __init__(self):
-        self.letterFrequencyDict = collections.defaultdict(int)
+        self.__letterFrequencyDict = collections.defaultdict(int)
         super().__init__()
 
     def process_line(self, line):
         for char in line:
             if re.compile('[A-Za-z]').match(char):
-                self.letterFrequencyDict[char.lower()] += 1
+                self.__letterFrequencyDict[char.lower()] += 1
 
     def calculate(self):
-        self.result = sorted(self.letterFrequencyDict,
-                             key=self.letterFrequencyDict.__getitem__,
+        self.result = sorted(self.__letterFrequencyDict,
+                             key=self.__letterFrequencyDict.__getitem__,
                              reverse=True)[0]
--- analyser/__init__.py
+++ analyser/__init__.py
@@ -1,1 +1,1 @@
-from analyser import TextFileAnalyser
+from analyser import TextFileAnalyser, Statistic
