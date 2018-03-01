ad084673f9ac0d29b0b9f169a96e7e970822c405
Simba-Cream <michaeldowdle@msn.com>
2018-03-01 20:27:23 +0000

    implement docstrings for Statistic module.

--- analyser/Statistic.py
+++ analyser/Statistic.py
@@ -5,12 +5,14 @@ import re
 
 
 class __StatisticBase(ABC):
-    """Private base class to use for creating statistics."""
+    """Private abstract base class to use for creating statistics."""
 
     def __init__(self, desc):
         """
+        Initializer sets a description of the type of statistic,
+        and a blank str result that will be set once calculated.
 
-        :param desc:
+        :param desc:    description of the type of statistic.
         """
         self.__description = desc
         self._result = ""
@@ -19,91 +21,104 @@ class __StatisticBase(ABC):
     @property
     def description(self):
         """
+        Property method for accessing private description attribute.
 
-        :return:
+        :return:        the description of the statistic.
         """
         return self.__description
 
     @property
     def result(self):
         """
+        Property method for accessing private result attribute.
 
-        :return:
+        :return:        the result calculated for the statistic.
         """
         return self._result
 
     @abstractmethod
     def process_line(self, line):
         """
+        Abstract method required to be defined by derived class
+        to take a str line as parameter and process it.
 
-        :param line:
-        :return:
+        :param line:    the line to be processed.
         """
         pass
 
     @abstractmethod
     def calculate(self):
         """
-
-        :return:
+        Abstract method required to be defined by derived class
+        to calculate a statistic result for lines processed.
         """
         pass
 
 
 class WordCountStatistic(__StatisticBase):
+    """Derived class for calculating word count statistic."""
+
     def __init__(self):
         """
-
+        Initializer sets a word count attribute as 0 and calls
+        based class initializer, providing a description.
         """
         self.__wordCount = 0
         super().__init__("whitespace delimited word count")
 
     def process_line(self, line):
         """
+        Method to process a line of data, adding the number
+        of words in the line to the private word count attribute.
 
-        :param line:
-        :return:
+        :param line:    the line of data to be processed.
         """
         self.__wordCount += len(line.split())
 
     def calculate(self):
+        """
+        Method to calculate statistic result, setting it to the
+        total word count attribute value.
+        """
         self._result = self.__wordCount
 
 
 class LineCountStatistic(__StatisticBase):
-    """
+    """Derived class for calculating line count statistic."""
 
-    """
     def __init__(self):
         """
-
+        Initializer sets a line count attribute as 0 and calls
+        based class initializer, providing a description.
         """
         self.__lineCount = 0
         super().__init__("line count")
 
     def process_line(self, *_):
         """
+        Method to process a line of data, incrementing the
+        private line count attribute.
 
-        :param _:
-        :return:
+        :param line:    the line of data to be processed.
         """
         self.__lineCount += 1
 
     def calculate(self):
         """
-
-        :return:
+        Method to calculate statistic result, setting it to the
+        total line count attribute value.
         """
         self._result = self.__lineCount
 
 
 class AvgLettersPerWordStatistic(__StatisticBase):
-    """
+    """Derived class for calculating average letters per word statistic."""
 
-    """
     def __init__(self):
         """
-
+        Initializer sets a word count attribute as 0, a
+        total word lengths attribute as 0 and calls
+        based class initializer, providing a description.
         """
         self.__wordCount = 0
         self.__totalWordLengths = 0
@@ -111,9 +126,12 @@ class AvgLettersPerWordStatistic(__StatisticBase):
 
     def process_line(self, line):
         """
+        Method to process a line of data, incrementing the
+        private word count attribute and adding the word
+        length to the private total word lengths attribute,
+        for each word in the line.
 
-        :param line:
-        :return:
+        :param line:    the line of data to be processed.
         """
         for word in line.split():
             self.__wordCount += 1
@@ -121,8 +139,8 @@ class AvgLettersPerWordStatistic(__StatisticBase):
 
     def calculate(self):
         """
-
-        :return:
+        Method to calculate statistic result, setting it to the
+        total word lengths divided by the total word count.
         """
         if self.__wordCount == 0:
             self._result = 0.0
@@ -131,21 +149,28 @@ class AvgLettersPerWordStatistic(__StatisticBase):
 
 
 class MostCommonLetterStatistic(__StatisticBase):
-    """
+    """Derived class for calculating most common letter statistic."""
 
-    """
     def __init__(self):
         """
-
+        Initializer sets a blank letter frequency dictionary attribute
+        using a default dictionary collection of type int, and calls
+        based class initializer, providing a description.
         """
         self.__letterFrequencyDict = collections.defaultdict(int)
         super().__init__("most common letter")
 
     def process_line(self, line):
         """
+        Method to process a line of data, incrementing a value for
+        each letters entry location in the private letter
+        frequency dictionary attribute. Inserting an entry if one
+        does not already exist for that letter.
 
-        :param line:
-        :return:
+        .. note::   entry is only added/incremented if character
+                    is a uppercase or lowercase letter.
+
+        :param line:    the line of data to be processed.
         """
         for char in line:
             if re.compile('[A-Za-z]').match(char):
@@ -153,12 +178,14 @@ class MostCommonLetterStatistic(__StatisticBase):
 
     def calculate(self):
         """
-
-        :return:
+        Method to calculate statistic result by sorting the letter
+        frequency dictionary by the value(letter count) in reverse
+        order, and then getting the key for the first entry in that
+        sorted dictionary.
         """
         if len(self.__letterFrequencyDict) == 0:
             self._result = ''
         else:
             self._result = sorted(self.__letterFrequencyDict,
-                                 key=self.__letterFrequencyDict.__getitem__,
-                                 reverse=True)[0]
+                                  key=self.__letterFrequencyDict.__getitem__,
+                                  reverse=True)[0]
